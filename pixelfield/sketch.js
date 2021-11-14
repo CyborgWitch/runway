21f94f1405029cf09a9b93334c183bbf40c1a828
CyborgWitch <69846224+CyborgWitch@users.noreply.github.com>
2021-11-14 19:14:09 +1100

    Changing polysynth values (cleaner note)

--- pixelfield/sketch.js
+++ pixelfield/sketch.js
@@ -158,7 +158,7 @@ function playSynth() {
     //polySynth.play("D4",  0.1, 0.6, 0.5)
   } else if (dwnld) {
 
-    polySynth.play("F4",  0.1, 0, 0.5);
+    polySynth.play("F4",  0, 0, 0.4);
     polySynth.play("A5",  0.05, 0.6, 0.4);
     polySynth.play("B5",  0.05, 0.8, 0.4);
     polySynth.play("C5",  0.05, 0.9, 0.5);

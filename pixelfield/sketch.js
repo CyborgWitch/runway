b09e16f8e33aee641d8700778d59e64fb49ce0a5
CyborgWitch <69846224+CyborgWitch@users.noreply.github.com>
2021-11-22 18:35:40 +1100

    Update sketch.js

--- pixelfield/sketch.js
+++ pixelfield/sketch.js
@@ -165,8 +165,8 @@ function playSynth() {
 
     polySynth.play("F4",  0.1, 0, 0.4)
     polySynth.play("A5",  0.1, 0.7, 0.4)
-    polySynth.play("B5",  0.1, 0.11, 0.4)
-    polySynth.play("C5",  0.1, 0.16, 0.5)
+    polySynth.play("B5",  0.1, 0.12, 0.3)
+    polySynth.play("C5",  0.1, 0.16, 0.4)
 
     //monoSynth.play win sound but in synth form/time.
   }

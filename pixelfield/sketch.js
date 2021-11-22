769180f4261c51d2482546a61347dc47ed9ae11b
CyborgWitch <69846224+CyborgWitch@users.noreply.github.com>
2021-11-23 09:32:20 +1100

    Update sketch.js

--- pixelfield/sketch.js
+++ pixelfield/sketch.js
@@ -3,15 +3,14 @@ let monoSynth;
 
 let clear = false;
 let dwnld = false;
-let field = false;
 
 //variable based on mouse speed
 let v;
 let speed;
 
 function setup() {
-  createCanvas(window.innerWidth, window.innerHeight);
-  //createCanvas(500, 500);
+  // createCanvas(window.innerWidth, window.innerHeight);
+  createCanvas(500, 500);
   background(random(150,255), random(150,255), random(150,255));
 
   polySynth = new p5.PolySynth();
@@ -35,8 +34,8 @@ function draw() {
 
   for (let i = 0; i <= width; i = i + 10) {
     for (let j = 0; j <= height; j = j + 10) {
-         line(i, 0, i, height);
-         line(0, j, width, j);
+      line(i, 0, i, height);
+      line(0, j, width, j);
     }
   }
 
@@ -59,12 +58,12 @@ function draw() {
     pop();
   pop();
 
-  // if mouse has been pressed within "clear"
+  // if mouse is pressed within "clear"
   if (clear) {
       //clear is true
       //redraw canvas: background, boxes, grid, etc. 
       push();
-        frameRate(3);
+        frameRate(5);
         background(random(150, 255), random(150, 255), random(150, 255));
       pop();
       push();
@@ -108,13 +107,16 @@ function draw() {
         rect(0.5, 0.5, width - 1, height - 1);
       pop();
     
-    // if mouse has been pressed inside "dwnld"
+    // if mouse is pressed inside "dwnld"
   } else if (dwnld) {
-    //dwnld is true, fill button
+    //dwnld is true
       push();
         fill(0, 50);
         rect(width - 80, height-29.5, 79.5, 29);
       pop();
+  } else {
+    clear = false;
+    dwnld = false;
   }
 }
 
@@ -122,48 +124,39 @@ function mousePressed() {
   userStartAudio();
   playSynth();
      //if mouse is pressed inside "clear"
-  if (mouseX > width - 80 && mouseX < width && mouseY > 0 && mouseY < 30) {
+  if (mouseX > width - 80 && mouseX < width && mouseY > 0 && mouseY < 30 && mouseIsPressed) {
       //clear is true
       console.log("clear");
       clear = true;
       dwnld = false;
-      field = false;
     
     //if mouse is pressed inside "dwnld"
-  } else if (mouseX > width - 80 && mouseX < width && mouseY > height - 30 && mouseY < height) {
-      dwnld = true;
-      clear = false;
-      field = false;
-      console.log("dwnld");
-      saveCanvas("myPixelField", "png");
+  } else if (mouseX > width - 80 && mouseX < width && mouseY > height - 30 && mouseY < height && mouseIsPressed) {
+     dwnld = true;
+     clear = false;
+     console.log("dwnld");
+     saveCanvas("myPixelField", "png");
     
-    //if mouse is pressed anywhere else
   } else {
-      field = true;
-      clear = false;
-      dwnld = false;
-      console.log("field");
-
+     clear = false;
+     dwnld = false;
   }
 }
 
 function playSynth() {
   //userStartAudio();
-  if (clear && !field && !dwnld) {
+  if (clear && !dwnld) {
      polySynth.play("G4",  0.1, 0, 0.3);
      polySynth.play("C4",  0.1, 0.4, 0.3);
-     clear = false;
     
-  } else if (dwnld && !field && !clear) {
+  } else if (dwnld && !clear) {
      //polySynth.play win sound but in synth form/time. 
      monoSynth.play("G4", 0.1, 0, 0.4);
      polySynth.play("C4", 0.1, 0.5, 0.3);
      polySynth.play("B5", 0.1, 0.8, 0.2);
      polySynth.play("C5", 0.1, 1, 0.2);
-     dwnld = false;
-    
     // mapped monosynth for pixelField
-  } else if (field && !dwnld && !clear) {
+  } else {
      let mapNote = int(map(mouseX, 0, width, 0, 5));
      let noteSelect = ["C4", "D4", "E4", "F4", "G4"];
      monoSynth.play(noteSelect[mapNote], 0.1, 0, 0.5);
@@ -172,9 +165,7 @@ function playSynth() {
 
 function mouseDragged() {
   userStartAudio();
-  playSynth();  
-  
-  //this is drawing the pixels as you drag
+  playSYnth();  
   for (let i = 0; i <= width; i = i + 20) {
     for (let j = 0; j <= height; j = j + 20) {
       if (mouseX > i && mouseX < i + 20 && mouseY > j && mouseY < j + 20) {
@@ -183,7 +174,6 @@ function mouseDragged() {
       }
     }
   }
-  
   //so the boxes draw OVER the drawing pixels
   push();
     fill(203, 194, 184);

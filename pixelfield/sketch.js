1ee07dea6edec2f40c576a124dd6630a8cc8b793
CyborgWitch <69846224+CyborgWitch@users.noreply.github.com>
2021-11-22 19:41:01 +1100

    Update sketch.js

--- pixelfield/sketch.js
+++ pixelfield/sketch.js
@@ -161,9 +161,9 @@ function playSynth() {
     //monoSynth.play("C5",  0.1, 0.13, 0.4);
     
     polySynth.play("F4",  0.1, 0, 0.4);
-    polySynth.play("A5",  0.1, 0.8, 0.4);
-    polySynth.play("B5",  0.1, 1.1, 0.2);
-    polySynth.play("C5",  0.1, 1.2, 0.1);
+    polySynth.play("A5",  0.1, 0.7, 0.5);
+    polySynth.play("B5",  0.1, 1, 0.2);
+    polySynth.play("C5",  0.1, 1.1, 0.1);
 
     // mapped monosynth for pixelField
   } else {

f12eebd46597eee4a55b1794857d55be9fe1e94e
CyborgWitch <69846224+CyborgWitch@users.noreply.github.com>
2021-11-22 18:15:19 +1100

    sequenced poly synth notes (no gap)

--- pixelfield/sketch.js
+++ pixelfield/sketch.js
@@ -160,8 +160,8 @@ function playSynth() {
 
     polySynth.play("F4",  0, 0, 0.4);
     polySynth.play("A5",  0.05, 0.6, 0.4);
-    polySynth.play("B5",  0.05, 0.8, 0.4);
-    polySynth.play("C5",  0.05, 0.9, 0.5);
+    polySynth.play("B5",  0.05, 0.10, 0.4);
+    polySynth.play("C5",  0.05, 0.14, 0.5);
 
     // polySynth.play("F4",  0.1, 0, 0.5)
     // polySynth.play("A5",  0.1, 0.3, 0.5)

983443ce221b6a3df239335410ea1407eff7ce3b
CyborgWitch <69846224+CyborgWitch@users.noreply.github.com>
2021-11-22 21:04:02 +1100

    Update sketch.js

--- pixelfield/sketch.js
+++ pixelfield/sketch.js
@@ -27,9 +27,9 @@ function draw() {
 
   //canvas perimenter border
   push();
-  strokeWeight(1);
-  noFill();
-  rect(0.5, 0.5, width - 1, height - 1);
+    strokeWeight(1);
+    noFill();
+    rect(0.5, 0.5, width - 1, height - 1);
   pop();
 
   for (let i = 0; i <= width; i = i + 10) {
@@ -41,102 +41,88 @@ function draw() {
 
   //interface boxes
   push();
-  fill(203, 194, 184);
-  //fill(0);
-  strokeWeight(1);
-  rect(width - 80, 0.5, 79.5, 29.5);
-  rect(width - 80, height - 29.5, 79.5, 29);
-  rect(0.5, 0.5, 269.5, 49.5);
-
-  push();
-  fill(0);
-  //fill(203, 194, 184);
-  textSize(22);
-  textLeading(22);
-  text("*clear*", width - 72, 22);
-  text("*dwnld*", width - 77, height - 7);
-  text("thanks for visiting \ndraw yourself a picture <*3", 5, 20);
-  pop();
-
-  pop();
-
-  if (
-    mouseX > width - 80 &&
-    mouseX < width &&
-    mouseY > 0 &&
-    mouseY < 30 &&
-    mouseIsPressed
-  ) {
-    //clear is true
-    console.log("clear");
-    clear = true;
-    dwnld = false;
-    //playSynth();
-    push();
-    frameRate(5);
-    background(random(150, 255), random(150, 255), random(150, 255));
-    pop();
-
-    push();
-    strokeWeight(1);
-    noFill();
-    rect(0.5, 0.5, width - 1, height - 1);
-    pop();
-
-
-    for (let i = 0; i <= width; i = i + 10) {
-      for (let j = 0; j <= height; j = j + 10) {
-        line(i, 0, i, height);
-        line(0, j, width, j);
-      }
-    }
-
-    push();
     fill(203, 194, 184);
     //fill(0);
     strokeWeight(1);
-    rect(width - 80, 0, 80, 30);
+    rect(width - 80, 0.5, 79.5, 29.5);
     rect(width - 80, height - 29.5, 79.5, 29);
     rect(0.5, 0.5, 269.5, 49.5);
+    push();
+      fill(0);
+      //fill(203, 194, 184);
+      textSize(22);
+      textLeading(22);
+      text("*clear*", width - 72, 22);
+      text("*dwnld*", width - 77, height - 7);
+      text("thanks for visiting \ndraw yourself a picture <*3", 5, 20);
+    pop();
+  pop();
 
+  // if mouse is pressed within "clear"
+  if (mouseX > width - 80 && mouseX < width && mouseY > 0 && mouseY < 30 && mouseIsPressed) {
+      //clear is true
+      console.log("clear");
+      clear = true;
+      dwnld = false;
+      playSynth();
+      
+      //redraw canvas: background, boxes, grid, etc. 
       push();
-      fill(0, 50);
-      rect(width - 80, 0, 80, 30);
+        frameRate(5);
+        background(random(150, 255), random(150, 255), random(150, 255));
+      pop();
+      push();
+        strokeWeight(1);
+        noFill();
+        rect(0.5, 0.5, width - 1, height - 1);
       pop();
 
-    push();
-    fill(0);
-    textSize(22);
-    textLeading(22);
-    text("*clear*", width - 72, 22);
-    text("*dwnld*", width - 77, height - 7);
-    text("thanks for visiting \ndraw yourself a picture <*3", 5, 20);
-    pop();
-    pop();
+      for (let i = 0; i <= width; i = i + 10) {
+        for (let j = 0; j <= height; j = j + 10) {
+          line(i, 0, i, height);
+          line(0, j, width, j);
+        }
+      }
+      push();
+        fill(203, 194, 184);
+        //fill(0);
+        strokeWeight(1);
+        rect(width - 80, 0, 80, 30);
+        rect(width - 80, height - 29.5, 79.5, 29);
+        rect(0.5, 0.5, 269.5, 49.5);
 
-    push();
-    strokeWeight(1);
-    noFill();
-    rect(0.5, 0.5, width - 1, height - 1);
-    pop();
-  } else if (
-    mouseX > width - 80 &&
-    mouseX < width &&
-    mouseY > height - 30 &&
-    mouseY < height &&
-    mouseIsPressed
-  ) {
+          push();
+          fill(0, 50);
+          rect(width - 80, 0, 80, 30);
+          pop();
+
+        push();
+          fill(0);
+          textSize(22);
+          textLeading(22);
+          text("*clear*", width - 72, 22);
+          text("*dwnld*", width - 77, height - 7);
+          text("thanks for visiting \ndraw yourself a picture <*3", 5, 20);
+        pop();
+      pop();
+
+      push();
+        strokeWeight(1);
+        noFill();
+        rect(0.5, 0.5, width - 1, height - 1);
+      pop();
+    
+    // if mouse is pressed inside "dwnld"
+  } else if (mouseX > width - 80 && mouseX < width && mouseY > height - 30 && mouseY < height && mouseIsPressed) {
     dwnld = true;
     clear = false;
     console.log("dwnld");
-    //playSynth();
+    playSynth();
     saveCanvas("myPixelField", "png");
-
       push();
-      fill(0, 50);
-      rect(width - 80, height-29.5, 79.5, 29);
+        fill(0, 50);
+        rect(width - 80, height-29.5, 79.5, 29);
       pop();
-
   } else {
     clear = false;
     dwnld = false;
@@ -146,10 +132,11 @@ function draw() {
 
 function playSynth() {
   //userStartAudio();
-  if (clear) {
+  if (clear && !dwnld) {
     polySynth.play("G4",  0.1, 0, 0.3);
     polySynth.play("C4",  0.1, 0.35, 0.2);
-  } else if (dwnld) {
+    
+  } else if (dwnld && !clear) {
     //polySynth.play win sound but in synth form/time. 
     polySynth.play("C4",  0.1, 0, 0.4);
     polySynth.play("A5",  0.1, 0.5, 0.9);
@@ -157,7 +144,7 @@ function playSynth() {
     polySynth.play("C5",  0.1, 1, 0.4);
 
     // mapped monosynth for pixelField
-  } else if (!clear && !dwnld) {
+  } else {
     let mapNote = int(map(mouseX, 0, width, 0, 5));
     let noteSelect = ["C4", "D4", "E4", "F4", "G4"];
     monoSynth.play(noteSelect[mapNote], 0.1, 0, 0.5);
@@ -175,23 +162,24 @@ function mouseDragged() {
       }
     }
   }
+  //so the boxes draw OVER the drawing pixels
   push();
-  fill(203, 194, 184);
-  //fill(0);
-  strokeWeight(1);
-  rect(width - 80, 0.5, 79.5, 29.5);
-  rect(width - 80, height - 29.5, 79.5, 29);
-  rect(0.5, 0.5, 269.5, 49.5);
+    fill(203, 194, 184);
+    //fill(0);
+    strokeWeight(1);
+    rect(width - 80, 0.5, 79.5, 29.5);
+    rect(width - 80, height - 29.5, 79.5, 29);
+    rect(0.5, 0.5, 269.5, 49.5);
 
-  push();
-  fill(0);
-  //fill(203, 194, 184);
-  textSize(22);
-  textLeading(22);
-  text("*clear*", width - 72, 22);
-  text("*dwnld*", width - 77, height - 7);
-  text("thanks for visiting \ndraw yourself a picture <*3", 5, 20);
-  pop();
+    push();
+      fill(0);
+      //fill(203, 194, 184);
+      textSize(22);
+      textLeading(22);
+      text("*clear*", width - 72, 22);
+      text("*dwnld*", width - 77, height - 7);
+      text("thanks for visiting \ndraw yourself a picture <*3", 5, 20);
+    pop();
   pop();
 }
 
